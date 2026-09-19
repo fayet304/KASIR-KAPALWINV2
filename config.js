5d2517063a8977d3b80c53fae55f825ded604179
fayet304 <ryukaizz03@gmail.com>
2026-09-19 17:09:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // =====================================================================
 
 // URL Web App hasil deploy Google Apps Script kamu
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx5wKeKNWI4MYgFnSMsa1J27YtQO1eNRtXrgxB_mixHkF6x4hsBfcgsTUOXRv8cCu1s/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxhfDckKGGcdpmTy8q0mxIltLKa2mgo9eBElMp6nOBfd6QrEpXmYYhsmJN-cqu66WIF/exec';
 
 // Link LINE CS buat tombol "Lupa sandi?" di halaman login
 const LINE_CS_URL = 'https://line.me/ti/p/GANTI_DENGAN_ID_LINE_CS';
